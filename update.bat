45c62f29085201a14d8ce5eb7a4226064ba71efa
elie-t <elie_t@hotmail.com>
2026-04-15 17:18:18 +0300

    Update script: clean untracked files before pull to prevent merge conflicts

--- update.bat
+++ update.bat
@@ -1,5 +1,8 @@
 @echo off
-echo Updating TannouryMarket POS...
+echo Checking for updates...
+git fetch origin
+git checkout -- .
+git clean -fd
 git pull
 echo.
 echo Update complete. Press any key to launch the app.
